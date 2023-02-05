82288d674b06a14e7d46bddf506bde7ef490bf77
Carlos Alexandre <carlos.alexandre@nos.pt>
2023-02-05 19:46:13 +0000

    added force destroy to the dns zone

--- bootstrap/dns.tf
+++ bootstrap/dns.tf
@@ -1,7 +1,8 @@
 resource "google_dns_managed_zone" "master" {
-  name        = "dns-master-zone"
-  dns_name    = "${var.master_zone_dns_name}."
-  description = "Master zone (managed by terraform)"
+  name          = "dns-master-zone"
+  dns_name      = "${var.master_zone_dns_name}."
+  description   = "Master zone (managed by terraform)"
+  force_destroy = true
 }
 
 resource "google_service_account" "dns_admin" {
